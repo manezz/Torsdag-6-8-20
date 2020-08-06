c33afd2fa76984a2f895c60b1190edebbddc00e1
Magnus <68356591+manezz@users.noreply.github.com>
2020-08-06 08:31:53 +0200

    Add project files.

---
+++ Torsdag-6-8-20/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torsdag_6_8_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tvkant = "\x250C";
+            string thkant = "\x2510";
+            string bvkant = "\x2510";
+            string bhkant = "\x2502";
+            string vside = "\x2502";
+            string hside = "\x2500";
+            string top = "\x2500";
+            string bund = "\x2500";
+        }
+    }
+}
